c0323ef06bf9d2b58b267fc04d38841d4bda0e67
Mavie10 <maviegrg@yahoo.com>
2023-11-15 15:40:18 -0800

    m

---
+++ main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * main - Entry point to simple shell program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *cmd;
+	char *parsed_cmd;
+	pid_t pid;
+
+	while (1)
+	{
+		printf("$ ");
+		cmd = get_cmd();
+
+	parsed_cmd = parse_cmd(cmd);
+	if (strlen(parsed_cmd) == 0)
+	{
+		free(cmd);
+		continue;
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+		exe_cmd(parsed_cmd);
+		exit(EXIT_FAILURE); /* execve only returns on error */
+	}
+	else if (pid > 0)
+	{
+		/* Parent process */
+		wait(NULL); /* Wait for the child process to complete */
+	}
+	else
+	{
+		/* Fork failed */
+		perror("Fork failed");
+	}
+
+	free(cmd);
+	}
+
+	return (0);
+}
+
---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* first task */
+char *get_cmd(void);
+char *parse_cmd(char *cmd);
+void exe_cmd(char *cmd);
+
+#endif
+
