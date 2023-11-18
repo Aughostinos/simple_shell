f475c3dfd2873222abd1e21920f0f2b2b459ffff
aughostinos <augustinos.nabil@gmail.com>
2023-11-17 17:45:58 -0800

    trial

--- main.c
+++ main.c
@@ -1,50 +1,70 @@
 #include "main.h"
 
-/**
- * main - Entry point to simple shell program
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char *cmd;
-	char *parsed_cmd;
-	pid_t pid;
+int main(int ac, char **argv){
+    char *prompt = "(Eshell) $ ";
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    size_t n = 0;
+    ssize_t nchars_read;
+    const char *delim = " \n";
+    int num_tokens = 0;
+    char *token;
+    int i;
 
-	while (1)
-	{
-		printf("$ ");
-		fflush(stdout);
-		cmd = get_cmd();
+    /* declaring void variables */
+    (void)ac;
 
-	parsed_cmd = parse_cmd(cmd);
-	if (strlen(parsed_cmd) == 0)
-	{
-		free(cmd);
-		continue;
-	}
+    /* Create a loop for the shell's prompt */
+    while (1) {
+        printf("%s", prompt);
+        nchars_read = getline(&lineptr, &n, stdin);
+        /* check if the getline function failed or reached EOF or user use CTRL + D */
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
 
-	pid = fork();
-	if (pid == 0)
-	{
-		/* Child process */
-		exe_cmd(parsed_cmd);
-		exit(EXIT_FAILURE); /* execve only returns on error */
-	}
-	else if (pid > 0)
-	{
-		/* Parent process */
-		wait(NULL); /* Wait for the child process to complete */
-	}
-	else
-	{
-		/* Fork failed */
-		perror("Fork failed");
-	}
+        /* allocate space for a copy of the lineptr */
+        lineptr_copy = malloc(sizeof(char) * nchars_read);
+        if (lineptr_copy== NULL){
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        /* copy lineptr to lineptr_copy */
+        strcpy(lineptr_copy, lineptr);
 
-	free(cmd);
-	}
+        /********** split the string (lineptr) into an array of words ********/
+        /* calculate the total number of tokens */
+	token = strtok(lineptr, delim);
 
-	return (0);
+        while (token != NULL){
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        num_tokens++;
+
+        /* Allocate space to hold the array of strings */
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        /* Store each token in the argv array */
+        token = strtok(lineptr_copy, delim);
+
+        for (i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+        /* execute the command */
+        execmd(argv);
+
+    }
+
+
+    /* free up allocated memory */
+    free(lineptr_copy);
+    free(lineptr);
+
+    return (0);
 }
-
--- main.h
+++ main.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-
-/* first task */
-char *get_cmd(void);
-char *parse_cmd(char *cmd);
-void exe_cmd(char *cmd);
-
-#endif
-
+void execmd(char **argv);
+#endif  
